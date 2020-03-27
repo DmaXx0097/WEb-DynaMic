2c6957fb23d0a35a876b0f7860c5470c73fffdac
Doman Murmu <55993713+DmaXx0097@users.noreply.github.com>
2020-03-27 17:08:52 +0530

    changes to font

--- js/main.js
+++ js/main.js
@@ -12,3 +12,8 @@ $(document).ready(function(){
 $(window).on("load",function(){
      $(".loader-wrapper").fadeOut("slow");
 });
+/*$('.down').click(function(){
+	$('html,body').animate({
+		$scrollTop:0
+	},1000);
+})*/
